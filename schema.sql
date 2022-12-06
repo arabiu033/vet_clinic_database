88f75a270884e15a4425f86f0e896e5538f07e73
Aminu Rabiu <arabiu033@gmail.com>
2022-12-06 16:35:41 +0100

    Modified the schema to add a column in table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Delete table if already exist
+DROP TABLE IF EXISTS animals;
+
+-- Create new table
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg FLOAT
+    weight_kg FLOAT,
+    species varchar(256) DEFAULT NULL
 );
