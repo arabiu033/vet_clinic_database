45e0aafca8332f3753cfed465985f22b7f8bea4b
Aminu Rabiu <arabiu033@gmail.com>
2022-12-07 20:50:42 +0100

    Add more joined tables

--- schema.sql
+++ schema.sql
@@ -1,9 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 -- Delete table if already exist
-DROP TABLE IF EXISTS animals;
-DROP TABLE IF EXISTS owners;
-DROP TABLE IF EXISTS species;
+DROP TABLE IF EXISTS animals CASCADE;
+DROP TABLE IF EXISTS owners CASCADE;
+DROP TABLE IF EXISTS species CASCADE;
+DROP TABLE IF EXISTS vets CASCADE;
+DROP TABLE IF EXISTS visits CASCADE;
+DROP TABLE IF EXISTS specializations CASCADE;
 
 -- animals owners table
 CREATE TABLE owners (
@@ -20,7 +23,7 @@ CREATE TABLE species (
     PRIMARY KEY (id)
 );
 
--- animals tables
+-- animals table
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
@@ -29,5 +32,28 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg FLOAT,
     species_id INT REFERENCES species(id),
-    owner_id INT REFERENCES owners(id)
+    owner_id INT REFERENCES owners(id),
+    PRIMARY KEY(ID)
+);
+
+-- vets table
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(256),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+-- Join table for vets and species
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+    vets_id INT REFERENCES vets(id)
+);
+
+-- JOI table for animals and vets
+CREATE TABLE visits (
+    animals_id INT REFERENCES animals(id),
+    vets_id INT REFERENCES vets(id),
+    date_of_visit DATE
 );
